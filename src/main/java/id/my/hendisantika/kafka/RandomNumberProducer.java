811582094b97252dfceafe70328addb307b9ec7e
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 18:19:48 +0700

    Add produce in RandomNumberProducer 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafka/RandomNumberProducer.java
+++ src/main/java/id/my/hendisantika/kafka/RandomNumberProducer.java
@@ -1,8 +1,16 @@
 package id.my.hendisantika.kafka;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.String.format;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,12 +23,22 @@ import org.springframework.stereotype.Component;
  * Time: 18.17
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class RandomNumberProducer {
 
     private static final int MIN = 10;
     private static final int MAX = 100_000;
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 1000)
+    public void produce() throws UnknownHostException {
+        int random = ThreadLocalRandom.current().nextInt(MIN, MAX);
+        this.kafkaTemplate.sendDefault(String.valueOf(random));
+        //just for logging
+        String hostName = InetAddress.getLocalHost().getHostName();
+        log.info(format("%s produced %d", hostName, random));
+    }
 }
