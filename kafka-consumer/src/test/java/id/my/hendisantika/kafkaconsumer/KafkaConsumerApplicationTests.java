a4cc55dfcc1759a7ceee4a6ab60fea71f6f77b64
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 18:21:57 +0700

    Create Kafka Consumer Project 🫸🌀✏️📗🐧🐳

---
+++ kafka-consumer/src/test/java/id/my/hendisantika/kafkaconsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
