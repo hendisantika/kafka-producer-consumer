eaa9f5f4f233bddab95f71a35c8cea997e190839
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 18:17:39 +0700

    @EnableScheduling 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafka/KafkaProducerConsumerApplication.java
+++ src/main/java/id/my/hendisantika/kafka/KafkaProducerConsumerApplication.java
@@ -2,8 +2,10 @@ package id.my.hendisantika.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class KafkaProducerConsumerApplication {
 
     public static void main(String[] args) {
