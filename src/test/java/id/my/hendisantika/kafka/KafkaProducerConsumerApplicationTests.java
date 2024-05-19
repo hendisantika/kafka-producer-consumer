01634032c315aeb70b4eb53df96d25520c43bc93
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 18:13:16 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/kafka/KafkaProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
